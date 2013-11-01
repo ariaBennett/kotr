023129df3638e4462260fd428ac12c77c9132a92
Aria Bennett <aria@aria-Aspire-8930.(none)>
2013-10-31 18:16:54 -0700

    Added Accounts schim

--- kotr/client/kotr.js
+++ kotr/client/kotr.js
@@ -1,17 +1,44 @@
+// Session variables
+Session.set("enabledContent", {});
 
-Session.set("showCalendar", false);
-
+// Template events
 Template.navbar.events({
   'click .navbar_button_calendar': function () {
-    if (Session.get("showCalendar", true)) {
-      Session.set("showCalendar", false);
+    var enabledContent = Session.get("enabledContent") ;
+    if (enabledContent["calendar"]) {
+      enabledContent["calendar"] = false ;
     }
     else {
-      Session.set("showCalendar", true);
+      enabledContent["calendar"] = true;
     }
+    Session.set("enabledContent", enabledContent) ;
+  } ,
+  'click .navbar_button_login': function () {
+    var enabledContent = Session.get("enabledContent") ;
+    if (enabledContent["account"]) {
+      enabledContent["account"] = false ;
+    }
+    else {
+      enabledContent["account"] = true;
+    }
+    Session.set("enabledContent", enabledContent) ;
   }
 });
 
+// Template functions
 Template.page.showCalendar = function () {
-  return Session.get("showCalendar");
+  return (Session.get("enabledContent"))["calendar"];
+}
+
+Template.page.showCalendarButtons = function () {
+  return (Session.get("enabledContent"))["calendarButtons"];
+}
+
+Template.page.showAccount = function () {
+  return (Session.get("enabledContent"))["account"];
+}
+
+// functions
+disableAll = function () {
+  Session.set("enabledContent", {})
 }
